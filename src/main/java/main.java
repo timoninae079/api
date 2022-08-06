6e9286719630afd84ff086a9853d60bdf92dd555
ЕленаПрекрасная <timoninae079@gmail.com>
2022-08-06 16:57:03 +0300

    Post

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,14 +1,14 @@
 public class main {
     public static void main(String[] args) {
 
-        post post = new post();
+        Post post = new Post();
 
         post.name = "Иван";
         post.passport = "4444 № 44444444";
         post.patronymic = "Иванович";
         post.phone = "+7 (999)-999-99-99";
         post.surname = "Иванов";
-        post.subscription = "true";
+        post.subscription = true;
 
         post.birthday = new FormDate();
         post.birthday.day = 13;
